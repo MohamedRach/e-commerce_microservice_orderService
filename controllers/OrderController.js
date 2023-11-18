657dfb81e6680e75b6ecd3bb57b7f6b62be02a69
MohamedRach <rachidi.mohamed.sadek@gmail.com>
2023-11-18 17:41:03 +0100

    order service completed

---
+++ controllers/OrderController.js
@@ -0,0 +1,22 @@
+
+const Order = require("../models/Orders")
+
+
+
+function createOrder(products, userEmail) {
+    let total = 0;
+    for (let t = 0; t < products.length; ++t) {
+        total += products[t].price;
+    }
+    const newOrder = new Order({
+        products,
+        user: userEmail,
+        total_price: total,
+    });
+    newOrder.save();
+    return newOrder;
+}
+
+module.exports = {
+    createOrder
+}
--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 require('dotenv').config()
 const express = require("express")
+const amqp = require("amqplib")
 const app = express()
 const mongoose = require("mongoose")
+const OrderController = require("./controllers/OrderController")
 //const routes = require("./routes/routes")
 mongoose.connect( process.env.dbURI)
     .then(() => {
         console.log("db connected successfully")
-        app.listen("7070" , () => {
+        app.listen("8080" , () => {
             console.log("listening on port 7070")
         })
     }).catch((err) => console.log(err))
 
 app.use(express.json())
+var channel, connection;
+async function connect() {
+    const amqpServer = "amqp://localhost:5672";
+    connection = await amqp.connect(amqpServer);
+    channel = await connection.createChannel();
+    await channel.assertQueue("ORDER");
+}
+connect().then(() => {
+    channel.consume("ORDER", (data) => {
+        console.log("Consuming ORDER service");
+        const { products, userEmail } = JSON.parse(data.content);
+        const newOrder = OrderController.createOrder(products, userEmail);
+        channel.ack(data);
+        console.log(newOrder)
+    });
+})
